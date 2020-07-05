2619a01d4b582e0bbb57b017e36540d1f9ad4295
Hokuto Shinoda <pinkumohikan+github@gmail.com>
2020-07-05 13:25:46 +0900

    refactor: Golang関連の設定をコンポーネントに切り出した

--- isucon3-mod/provision/roles/app.rb
+++ isucon3-mod/provision/roles/app.rb
@@ -18,26 +18,8 @@ service "mysqld" do
   action [:enable, :start]
 end
 
-
 # Golang
-["tar", "git"].each do |p| package p end
-# NOTE: amazon-linux-extrasで1.14が入らないので手動でバイナリ取ってくる
-execute "install golang" do
-  command <<'CMD'
-    curl -o /tmp/go1.14.4.linux-amd64.tar.gz -L https://golang.org/dl/go1.14.4.linux-amd64.tar.gz && \
-    tar -C /usr/local -xzf /tmp/go1.14.4.linux-amd64.tar.gz && \
-    rm /tmp/go1.14.4.linux-amd64.tar.gz
-CMD
-  not_if "test -f /usr/local/go/bin/go"
-end
-file "/etc/profile" do
-  action :edit
-  block do |content|
-    content.concat("\n", "export PATH=$PATH:/usr/local/go/bin")
-  end
-  not_if "grep \"/usr/local/go/bin\" /etc/profile"
-end
-
+include_recipe "../cookbooks/golang"
 
 # Application
 include_recipe "../cookbooks/application"
