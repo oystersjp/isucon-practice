67745c75e5b356e3cca3869b427ab088369a7f65
IkezoeMakoto <150283t@gmail.com>
2020-07-10 17:43:21 +0900

    benchにpigzが必要だった

--- isucon3-mod/provision/cookbooks/bench/default.rb
+++ isucon3-mod/provision/cookbooks/bench/default.rb
@@ -2,3 +2,12 @@ remote_directory "/opt/isucon/data" do
   action :create
   source :auto
 end
+
+# pigz
+execute "install epel repository" do
+  command "amazon-linux-extras install -y epel"
+  not_if "test -f /etc/yum.repos.d/epel.repo"
+end
+package "pigz" do
+  options "--enablerepo=epel"
+end
